b79e79226d63495ef816b3a2bc3cbe4599111cb2
hunt-22 <huntbsnl@gmail.com>
2025-08-18 00:00:50 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello from Jenkins + Vault automated deploy demo!")
+git clone https://github.com/hunt-22/dummyrepo.git
+cd dummyrepo
+
+# Create files as above (with nano/vim/editor)
+# Or, copy above content and save to app.py, deploy.sh, etc.
+
+git add .
+git commit -m "Added demo app, deploy script and readme for Jenkins + Vault pipeline"
+git push
